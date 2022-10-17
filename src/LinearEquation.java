705093da955a5af61e76ccb7c648b8c9e4de589e
SikderSakhawat <114007137+SikderSakhawat@users.noreply.github.com>
2022-10-17 10:01:52 -0400

    among us

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class LinearEquation {
     private double intX;
     private double intY;
@@ -36,8 +37,41 @@ public class LinearEquation {
         return result;
     }
 
-    public int intFromOrderedPair(String orderedPair){
+    public int intXFromOrderedPair(String orderedPair){
        String noParentheses = orderedPair.substring(1, orderedPair.length() - 1);
-       int whereComma = orderedPair.indexOf();
+       int whereComma = orderedPair.indexOf(",");
+       String firstNum = orderedPair.substring(1, whereComma);
+       String secondNum = orderedPair.substring(whereComma + 1, orderedPair.length() - 1);
+       int xNum = Integer.parseInt(firstNum);
+       int yNum = Integer.parseInt(secondNum);
+       return xNum;
+    }
+    public int intYFromOrderedPair(String orderedPair){
+        String noParentheses = orderedPair.substring(1, orderedPair.length() - 1);
+        int whereComma = orderedPair.indexOf(",");
+        String firstNum = orderedPair.substring(1, whereComma);
+        String secondNum = orderedPair.substring(whereComma + 1, orderedPair.length() - 1);
+        int xNum = Integer.parseInt(firstNum);
+        int yNum = Integer.parseInt(secondNum);
+        return yNum;
+    }
+    public void toString(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter an ordered pair: ");
+        String firstPair = s.nextLine();
+        System.out.println("Enter another ordered pair: ");
+        String secondPair = s.nextLine();
+        System.out.println("First pair: " + firstPair);
+        System.out.println("Second pair: " + secondPair);
+        intX = intXFromOrderedPair(firstPair);
+        intY = intYFromOrderedPair(firstPair);
+        finX = intXFromOrderedPair(secondPair);
+        finY = intYFromOrderedPair(secondPair);
+        System.out.print("Slope of line: ");
+        slopeInterceptFormula();
+        System.out.println("Slope of line: " + findSlope());
+        System.out.println("Y-intercept: " + findYIntercept());
+        System.out.print("Distance between points:" );
+        System.out.println(Math.sqrt(Math.pow()));
     }
 }
---
+++ src/U2ProjectRunner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class U2ProjectRunner {
+    public static void main(String[] args) {
+        LinearEquation myEqu = new LinearEquation(0,0,0,0); // initialized and will be changed later
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter an ordered pair: ");
+        String firstPair = s.nextLine();
+        System.out.println("Enter another ordered pair: ");
+        String secondPair = s.nextLine();
+
+    }
+}
